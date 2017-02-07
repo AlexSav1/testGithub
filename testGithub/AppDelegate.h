310eefce946e0cc7d9cddcda650743065083f10e
Aditya Narayan <savittalex@gmail.com>
2017-02-07 10:27:22 -0500

    Initial Commit

---
+++ testGithub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGithub
+//
+//  Created by Aditya Narayan on 2/7/17.
+//  Copyright © 2017 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGithub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGithub
+//
+//  Created by Aditya Narayan on 2/7/17.
+//  Copyright © 2017 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
