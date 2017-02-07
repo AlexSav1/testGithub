51366697999c8b024367e5ff0e85127866c6419c
Aditya Narayan <savittalex@gmail.com>
2017-02-07 10:29:54 -0500

    added animal

---
+++ testGithub/Animal.h
@@ -0,0 +1,15 @@
+//
+//  Animal.h
+//  testGithub
+//
+//  Created by Aditya Narayan on 2/7/17.
+//  Copyright © 2017 Aditya Narayan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Animal : NSObject
+
+@property NSString *species;
+
+@end
---
+++ testGithub/Animal.m
@@ -0,0 +1,13 @@
+//
+//  Animal.m
+//  testGithub
+//
+//  Created by Aditya Narayan on 2/7/17.
+//  Copyright © 2017 Aditya Narayan. All rights reserved.
+//
+
+#import "Animal.h"
+
+@implementation Animal
+
+@end
